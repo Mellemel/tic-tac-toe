99990265085f21c861f01fa0ec73a57c1276d38c
Melvin Canela <M.Canela92@outlook.com>
2016-09-11 22:25:54 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import MainMenu from 'react'
+import Stage from 'stage'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = ({ route: window.location.hash.substr(1) })
+  }
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: window.location.hash.substr(1)
+      })
+    })
+  }
+
+  render() {
+    let 
+    
+    switch (this.state.route) {
+      case '/': Child = About; break;
+      case '/inbox': Child = Inbox; break;
+      default: Child = MainMenu;
+    }
+
+    return (
+      <div className='container'>
+        <Child />
+      </div>
+    )
+  }
+}
+
+export default App
